7bc6f18182cb9f9d2674b1f70ca1f3b05f0a0335
jaytech504 <jasonyakubu543@gmail.com>
2023-04-09 05:43:41 +0100

    Django blog

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+posts = [
+        {
+        'author' : 'Jason',
+        'title' : 'My first site',
+        'content' : 'Hi everyone',
+        'date_posted' : 'March 8, 2023'
+        },
+        {
+        'author' : 'Mich',
+        'title' : 'Blog post',
+        'content' : 'Am a mumu',
+        'date_posted' : 'March 9, 2023'
+        }
+]
+
+def home(request):
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog/home.html', context)
+  
+def about(request):
+    return render(request, 'blog/about.html',{'title' : 'About'})
