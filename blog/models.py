e2fce21143174e05ac5ee20d73f307263949e53a
jaytech504 <88377613+jaytech504@users.noreply.github.com>
2023-04-09 09:24:44 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+#this is the models for each
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
